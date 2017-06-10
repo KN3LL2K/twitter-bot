a1d0e84ab38c8e0234da5eba97b341c0da83e161
Harry Bellenie <harrybellenie@gmail.com>
2017-06-09 22:47:38 -0700

    cleans up code

--- app.js
+++ app.js
@@ -28,18 +28,15 @@ T.stream('statuses/filter', params, function(stream) {
     var currency = tweet.text.split(' ')[1].toLowerCase();
     var user = tweet.user.screen_name
     if (currency !== undefined && currency in currencies) {
-      console.log(currency);
       var from = currencies[currency];
-
       request.get(`https://min-api.cryptocompare.com/data/price?fsym=${from}&tsyms=USD`).end(function(err, res) {
-        console.log(res.body.USD);
         var value = res.body.USD || 0;
         if (!err) {
           T.post('statuses/update', {status: `@${user} have you heard about ${currency}? It's $${value}.`}, function(error, tweet, response) {
             if (error) {
               console.log(error);
             }
-            console.log(tweet.in_reply_to_screen_name, currency, value);
+            console.log(tweet.text + ' sent to ' + tweet.in_reply_to_screen_name);
           })
         } else {
           console.log(err);
@@ -48,14 +45,13 @@ T.stream('statuses/filter', params, function(stream) {
     } else {
       console.log('nada');
       T.post('statuses/update', {status: `@${user} http://gph.is/1nFH9lL`}, function(error, tweet, response) {
-            if (error) {
-              console.log(error);
-            }
-            console.log(tweet.in_reply_to_screen_name);
-          })
+        if (error) {
+          console.log(error);
+        }
+        console.log(tweet.in_reply_to_screen_name);
+      })
     }
   })
-
   stream.on('error', function(error) {
     //print out the error
     console.log(error);
